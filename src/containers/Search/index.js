250ae063c8ab150a76a4ba0c96f35cd5b4277ccf
Suraj Verma <surajverma@rentalcars.com>
2020-08-28 19:11:50 +0100

    fetch data from server

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import SearchBar from "../../components/SearchBar";
 import Table from "../../components/Table";
 
+const API_URL = "https://randomuser.me/api/?results=10&nat=gb";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employees: [],
+      loading: true,
+      error: "",
+    };
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.get(API_URL);
+    const employees = data.results;
+
+    this.setState({
+      employees,
+      loading: false,
+      error: "",
+    });
+  }
+
+  renderTable() {
+    const { loading, error } = this.state;
+    if (!loading && !error) {
+      return <Table />;
+    }
+    return null;
+  }
+
   render() {
     return (
       <div className="container">
         <SearchBar />
-        <Table />
+        {this.renderTable()}
       </div>
     );
   }
