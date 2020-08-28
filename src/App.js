77240d72106ad436b641231c6fd39f12d1028b89
Suraj Verma <surajverma@rentalcars.com>
2020-08-28 18:57:31 +0100

    add Table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Jumbo from "./components/Jumbo";
 import Search from "./components/Search";
+import Table from "./components/Table";
 
 const App = () => {
   return (
     <div className="text-center">
       <Jumbo />
       <Search />
+      <Table />
     </div>
   );
 };
---
+++ src/components/Table/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>image</td>
+            <td>name</td>
+            <td>phoneNumber</td>
+            <td>email</td>
+            <td>dob</td>
+          </tr>
+          <tr>
+            <td>image</td>
+            <td>name</td>
+            <td>phoneNumber</td>
+            <td>email</td>
+            <td>dob</td>
+          </tr>
+          <tr>
+            <td>image</td>
+            <td>name</td>
+            <td>phoneNumber</td>
+            <td>email</td>
+            <td>dob</td>
+          </tr>
+          <tr>
+            <td>image</td>
+            <td>name</td>
+            <td>phoneNumber</td>
+            <td>email</td>
+            <td>dob</td>
+          </tr>
+          <tr>
+            <td>image</td>
+            <td>name</td>
+            <td>phoneNumber</td>
+            <td>email</td>
+            <td>dob</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
