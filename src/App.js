de3fdc1b8fef3d2423106df134a4b35cfabdf747
Suraj Verma <surajverma@rentalcars.com>
2020-08-28 18:52:37 +0100

    add Search component basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Jumbo from "./components/Jumbo";
+import Search from "./components/Search";
 
 const App = () => {
   return (
-    <div>
+    <div className="text-center">
       <Jumbo />
+      <Search />
     </div>
   );
 };
---
+++ src/components/Search/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div class="form-group mx-2">
+      <input type="text" class="form-control" placeholder="Search Employee" />
+    </div>
+  );
+};
+
+export default Search;
